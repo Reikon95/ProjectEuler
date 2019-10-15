74a0311775d92ce7e98e536c0761124c9523a8ce
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-10-15 16:27:32 +0100

    fixed ispalidrome funciton

--- 4.js
+++ 4.js
@@ -1,14 +1,17 @@
 function isPalidrome(num) {
   let str = String(num);
   let checker = 0
-  let x = (str.length - 1);
+  let backindex = (str.length - 1);
   for (let i = 0; i < str.length; i++) {
-    str[i] === str[x] ? checker++ : checker--;
-    x--;
+    console.log(i);
+    console.log(backindex);
+    str[i] === str[backindex] ? checker++ : checker--;
+    backindex--;
   }
-  return checker === 6 ? true : false;
+  return checker === str.length ? true : false;
 }
-console.log(isPalidrome(906609))
+console.log(isPalidrome(0000))
+
 
 function largestPalindromeProduct(n) {
   return true;
