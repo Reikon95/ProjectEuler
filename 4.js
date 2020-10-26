967049fd8606ba2c5055d0df19dd8b26910838d1
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2020-10-26 16:49:36 +0000

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,34 +1,24 @@
-function isPalidrome(num) {
-  let str = String(num);
-  let checker = 0
-  let backindex = (str.length - 1);
-  for (let i = 0; i < str.length; i++) {
-    console.log(i);
-    console.log(backindex);
-    str[i] === str[backindex] ? checker++ : checker--;
-    backindex--;
-  }
-  return checker === str.length ? true : false;
-}
-console.log(isPalidrome(0000))
-
-
-function findPalidromeProduct(n) {
-  let arr = [1]
+function largestPalindromeProduct(n) {
+  let startingInt = [];
+  let num = null
   for (let i = 0; i < n; i++) {
-    arr.push(0)
+    startingInt.push('9');    
   }
-  let value = (Number(arr.join('')) - 1);
-  for (let i = value; i > 0; i--) {
-    console.log(i)
-    let sum = Number(i * value);
-    if (isPalidrome(sum)) {
-      return sum;
-    } 
+  console.log(startingInt)
+  num = Number(startingInt.join(''))
+  for (let i = num; i > 0; i--) {
+    let result = String(i * num);
+    let reversed = result.split('').reverse().join('');
+    if (result === reversed) {
+      console.log(result, reversed)
+      return Number(result);
+    }
   }
 }
 
-console.log(findPalidromeProduct(3));
+
+
+largestPalindromeProduct(3);
 
 
 //UNFINISHED
