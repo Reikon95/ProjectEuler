ee2a5e3033e8a70443bc99d357748d7fbbc4f6ab
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-04-15 17:58:02 +0100

    update problem 3

--- 3.js
+++ 3.js
@@ -1,29 +1,29 @@
 function isPrime(num) {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return false;
+      return false
     }
   }
 }
 
 function largestPrimeFactor(number) {
-  let factors = [];
-  let primefactors = [number];
+  let factors = []
+  let primefactors = [number]
   for (let i = 2; i < number; i++) {
     if (Number.isInteger(number / i)) {
-      factors.push(i);
+      factors.push(i)
     }
   }
   for (let elem of factors) {
     if (isPrime(elem)) {
-      primefactors.push(elem);
-    } 
-  return primefactors[primefactors.length - 1];
+      primefactors.push(elem)
+    }
+  }
+  return primefactors[primefactors.length - 1]
 }
+console.log(largestPrimeFactor(13195))
+return largestPrimeFactor(13195)
 
-console.log(largestPrimeFactor(7));
-
-// Firstly, I know I can do this in less lines of code but I got the thing to work finally ;) pls no hate etc etc
 // 1) create a function to determine if number is prime
 // 2) create a second function
 // 3) create two arrays, one to store factors and one to store prime factors (one is always the inital number)
@@ -31,4 +31,4 @@ console.log(largestPrimeFactor(7));
 // 5) iterate through the factors to find which is prime
 // 6) return the last element of the prime factors array
 
-//Problem solved 08/10/2019 by a very tired Cam.
+//UNSOLVED
