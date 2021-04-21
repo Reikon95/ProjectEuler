83f7f09a206113d5bdac5c6a8921663822d215e6
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-04-21 12:52:14 +0100

    full test suite (prob 1)

--- Problem 1/1.test.js
+++ Problem 1/1.test.js
@@ -1,5 +1,18 @@
 const func3and5 = require("./1")
 
-test("adds 1 + 2 to equal 3", () => {
+test("primes", () => {
   expect(func3and5(10)).toBe(23)
+})
+
+test("primes", () => {
+  expect(func3and5(49)).toBe(543)
+})
+test("primes", () => {
+  expect(func3and5(1000)).toBe(233168)
+})
+test("primes", () => {
+  expect(func3and5(8456)).toBe(16687353)
+})
+test("primes", () => {
+  expect(func3and5(19564)).toBe(89301183)
 })
