71ab91ffa39d356d961d1e0a69a78767e438f9a5
Cameron <cameron.blackwood@purplebricks.com>
2021-11-26 13:08:31 +0000

    problem 2 fi

--- problem-2/2.js
+++ problem-2/2.js
@@ -1,16 +1,11 @@
-function fiboEvenSum(n) {
-  let result = [1, 2]
-  for (let i = 0; i < n - 1; i++) {
-    let sum = result[result.length - 1] + result[result.length - 2]
-    result.push(sum)
+function fiboEvenSum(maximumInt) {
+  let result = [1, 2];
+  const exitInt = maximumInt - 2;
+  for (let i = 0; i < exitInt; i++) {
+    let sum = result[result.length - 1] + result[result.length - 2];
+    result.push(sum);
   }
-  let counter = 0
-  for (let num of result) {
-    if (num % 2 === 0) {
-      counter += num
-    }
-  }
-  return counter
+  return result.filter((int) => int % 2 === 0 && int < maximumInt).reduce((a, b) => a + b);
 }
 
-module.exports = fiboEvenSum
+module.exports = fiboEvenSum;
--- problem-2/2.test.js
+++ problem-2/2.test.js
@@ -3,3 +3,23 @@ const fiboEvenSum = require("./2")
 test("10", () => {
   expect(fiboEvenSum(10)).toBe(10)
 })
+
+test("34", () => {
+  expect(fiboEvenSum(44)).toBe(44)
+})
+
+test("60", () => {
+  expect(fiboEvenSum(44)).toBe(44)
+})
+
+test("1000", () => {
+  expect(fiboEvenSum(798)).toBe(798)
+})
+
+test("10000", () => {
+  expect(fiboEvenSum(60696)).toBe(60696)
+})
+
+test("4000000", () => {
+  expect(fiboEvenSum(4613732)).toBe(4613732)
+})
